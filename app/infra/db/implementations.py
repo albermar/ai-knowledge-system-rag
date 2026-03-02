ae3593888fbe51bb5cd5f688691fbf6adfc154c2
Alberto Bermejillo Martín-Romo <alberto.bmoi@gmail.com>
2026-03-02 15:33:33 +0100

    use case wiring, update implementations

--- app/infra/db/implementations.py
+++ app/infra/db/implementations.py
@@ -3,14 +3,14 @@ from sqlalchemy.orm import Session
 from app.domain.interfaces import DocumentRepositoryInterface, OrganizationRepositoryInterface, QueryRepositoryInterface, ChunkRepositoryInterface, LLMUsageRepositoryInterface, QueryChunkRepositoryInterface
 from app.infra.db.ormmodels import Organization, Document, Query, Chunk, LLMUsage, QueryChunk
 
-class PostgreSQL_OrganizationRepositoryInterface(OrganizationRepositoryInterface):
+class PostgreSQL_OrganizationRepository(OrganizationRepositoryInterface):
     def __init__(self, db_session: Session):
         self.db_session = db_session
 
-class PostgreSQL_DocumentRepositoryInterface(DocumentRepositoryInterface):
+class PostgreSQL_DocumentRepository(DocumentRepositoryInterface):
     def __init__(self, db_session: Session):
         self.db_session = db_session
 
-class PostgreSQL_ChunkRepositoryInterface(ChunkRepositoryInterface):
+class PostgreSQL_ChunkRepository(ChunkRepositoryInterface):
     def __init__(self, db_session: Session):
         self.db_session = db_session
--- app/infra/parser/implementations.py
+++ app/infra/parser/implementations.py
@@ -3,4 +3,4 @@ from app.domain.interfaces import PDFParserInterface
 
 class V1_PDFParser(PDFParserInterface):
     def parse_pdf(self, file_content: bytes) -> str:
-        pass       
+        pass
